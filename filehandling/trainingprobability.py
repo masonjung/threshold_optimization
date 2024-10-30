a4da7cfc73a5cf6768edbbdec108fadafd55436d
Minseok "Mason" Jung <msjung@mit.edu>
2024-10-30 16:17:40 -0400

    file handling

---
+++ filehandling/trainingprobability.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the data from the file
+df = pd.read_csv('C:\\Users\\minse\\Desktop\\Programming\\FairThresholdOptimization\\datasets\\Training_dataset\\Train_RAID_MAGE.csv')
+df.shape
