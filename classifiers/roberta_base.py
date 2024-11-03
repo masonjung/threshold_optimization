b726f0a7ff3fb026241d7942eeb33df5a2dc4732
masonjung <msjung@mit.edu>
2024-11-03 16:55:33 -0500

    Added updates to roberta_base.py, test.py, textlength.py, sentiment.py, and training.py

--- classifiers/roberta_base.py
+++ classifiers/roberta_base.py
@@ -1,1 +1,60 @@
 # roberta based code
+
+import pandas as pd
+import os
+import re
+import gc
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import statsmodels.api as sm
+
+import torch
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+from huggingface_hub import login
+
+# Log in to Hugging Face with your token
+token = "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"
+login(token)
+
+# Load the tokenizer and model
+model_name = 'openai-community/roberta-base-openai-detector'
+tokenizer = RobertaTokenizer.from_pretrained(model_name)
+model2 = RobertaForSequenceClassification.from_pretrained(model_name)
+
+# Ensure the model is in evaluation mode
+model2.eval()
+
+def classify_text2(text):
+    inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True)
+    with torch.no_grad():
+        outputs = model2(**inputs)
+    logits = outputs.logits
+    predictions = torch.softmax(logits, dim=-1).tolist()[0]
+    # probabilities[0] corresponds to "human-written"
+    # probabilities[1] corresponds to "AI-generated"
+    return predictions
+
+# Clasisification
+def roberta_base_openai_detector_probability(text):
+    probabilities = classify_text2(text)
+    ai_probability = probabilities[1]  # Probability of being AI-generated
+    return ai_probability
+
+
+#test dataset
+dataset = pd.read_csv("C:\\Users\\minse\\Desktop\\Programming\\FairThresholdOptimization\\datasets\\Test_dataset\\test_ra_rl.csv")
+
+
+dataset['roberta_base_openai_detector_probability'] = dataset['essay'].apply(roberta_base_openai_detector_probability)
+
+dataset.columns
+
+# Save the dataset with the new column
+dataset.to_csv("C:\\Users\\minse\\Desktop\\Programming\\FairThresholdOptimization\\datasets\\Test_dataset\\test_d3.csv", index=False)
+
---
+++ datasets/test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Load the dataset
+dataset = pd.read_csv("C:\\Users\\minse\\Desktop\\Programming\\FairThresholdOptimization\\datasets\\Test_dataset\\test_d3.csv")
+
+# Display the first few rows of the dataset
+print(dataset.head())
