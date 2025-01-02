9b44b78965f25746d996d23cafed2e2afabf067c
masonjung <msjung@mit.edu>
2025-01-02 15:28:16 -0500

    relaxation try

--- optimization/FairOPT/FairOPT_old.py
+++ optimization/FairOPT/FairOPT_old.py
@@ -258,7 +258,7 @@ optimizer = ThresholdOptimizer(
     initial_thresholds,
     learning_rate=10**-2,
     max_iterations=10**4,
-    acceptable_disparity=0.2,  # Adjust based on your fairness criteria
+    acceptable_disparity=0.1,  # Adjust based on your fairness criteria
     min_acc_threshold=0.5,         # Set realistic minimum accuracy
     min_f1_threshold=0.5,           # Set realistic minimum F1 score
     tolerance=1e-4,  # Decrease tolerance for stricter convergence criteria
@@ -364,7 +364,7 @@ for source in unique_sources:
 
         feature_discrepancies = calculate_discrepancies(test_y_true, test_y_pred, features)
 
-        with open("C:\\Users\\minse\\Desktop\\Programming\\FairThresholdOptimization\\results_updated_0.2.txt", "a") as f:
+        with open("C:\\Users\\minse\\Desktop\\Programming\\FairThresholdOptimization\\results_updated_0.1.txt", "a") as f:
             f.write(f"\nPerformance for Source: {source}, Detector: {detector}\n")
             f.write(f"Accuracy: {test_accuracy:.4f}\n")
             f.write(f"False Positive Rate (FPR): {test_fpr:.4f}\n")
