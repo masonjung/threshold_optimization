04d14b9fa2f16a174ac6d010eb181df28b9aefa5
masonjung <msjung@mit.edu>
2025-01-03 01:18:12 -0500

    less penalty in F1

--- optimization/FairOPT/FairOPT_old.py
+++ optimization/FairOPT/FairOPT_old.py
@@ -97,6 +97,7 @@ class ThresholdOptimizer:
                 abs(self.thresholds[group] - previous_thresholds[group]) for group in self.thresholds
             )
             if max_threshold_change < self.tolerance:
+                # convergence criteria
                 if self.check_fairness(confusion_matrix_df) and self.check_performance_criteria(acc_dict, f1_dict):
                     print(f"Converged after {iteration} iterations.")
                     break
@@ -257,12 +258,12 @@ optimizer = ThresholdOptimizer(
     groups,
     initial_thresholds,
     learning_rate=10**-2,
-    max_iterations=10**4,
-    acceptable_disparity=0.1,  # Adjust based on your fairness criteria
+    max_iterations=10**5,
+    acceptable_disparity=0.2,  # Adjust based on your fairness criteria
     min_acc_threshold=0.5,         # Set realistic minimum accuracy
     min_f1_threshold=0.5,           # Set realistic minimum F1 score
     tolerance=1e-4,  # Decrease tolerance for stricter convergence criteria
-    penalty=20  # Increase penalty to enforce stricter updates
+    penalty=10  # Increase penalty to enforce stricter updates
 )
 
 # Optimize thresholds using gradient-based method
@@ -349,7 +350,7 @@ for source in unique_sources:
         test_y_pred = np.zeros_like(test_y_true)
         for group in np.unique(test_groups):
             group_indices = (test_groups == group)
-            threshold = thresholds.get(group, 0.2)  # Use thresholds dictionary
+            threshold = thresholds.get(group, 0.5)  # Use thresholds dictionary
             test_y_pred[group_indices] = test_y_pred_proba[group_indices] >= threshold
 
         test_accuracy = accuracy_score(test_y_true, test_y_pred)
@@ -364,7 +365,7 @@ for source in unique_sources:
 
         feature_discrepancies = calculate_discrepancies(test_y_true, test_y_pred, features)
 
-        with open("C:\\Users\\minse\\Desktop\\Programming\\FairThresholdOptimization\\results_updated_0.1.txt", "a") as f:
+        with open("C:\\Users\\minse\\Desktop\\Programming\\FairThresholdOptimization\\results_updated_0.2_10K_p10.txt", "a") as f:
             f.write(f"\nPerformance for Source: {source}, Detector: {detector}\n")
             f.write(f"Accuracy: {test_accuracy:.4f}\n")
             f.write(f"False Positive Rate (FPR): {test_fpr:.4f}\n")
