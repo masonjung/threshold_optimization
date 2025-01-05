d40815c88119480f7f9f203c45b2929acd9bc8be
masonjung <msjung@mit.edu>
2025-01-04 22:46:37 -0500

    fair opt update

--- optimization/FairOPT/FairOPT_old.py
+++ optimization/FairOPT/FairOPT_old.py
@@ -13,7 +13,7 @@ class ThresholdOptimizer:
         groups,
         initial_thresholds,
         learning_rate=10**-2,   # Adjusted learning rate
-        max_iterations=10**5,
+        max_iterations=10**4,
         acceptable_disparity=0.2,
         min_acc_threshold=0.5,
         min_f1_threshold=0.5,  # F1 is for stable performance
@@ -135,7 +135,7 @@ class ThresholdOptimizer:
         fpr_disparity = fpr_values.max() - fpr_values.min()
         tpr_disparity = tpr_values.max() - tpr_values.min()
 
-        # DP
+        # DP and EO
         if ppr_disparity <= self.acceptable_ppr_disparity and (fpr_disparity <= self.acceptable_fpr_disparity and tpr_disparity <= self.acceptable_tpr_disparity):
             return True
         else:
@@ -258,8 +258,8 @@ optimizer = ThresholdOptimizer(
     groups,
     initial_thresholds,
     learning_rate=10**-2,
-    max_iterations=10**3,
-    acceptable_disparity=0.1,  # Adjust based on your fairness criteria
+    max_iterations=10**5,
+    acceptable_disparity=0.2,  # Adjust based on your fairness criteria
     min_acc_threshold=0.5,         # Set realistic minimum accuracy
     min_f1_threshold=0.5,           # Set realistic minimum F1 score
     tolerance=1e-4,  # Decrease tolerance for stricter convergence criteria
@@ -365,7 +365,7 @@ for source in unique_sources:
 
         feature_discrepancies = calculate_discrepancies(test_y_true, test_y_pred, features)
 
-        with open("C:\\Users\\minse\\Desktop\\Programming\\FairThresholdOptimization\\results_updated_0.1_rv.txt", "a") as f:
+        with open("C:\\Users\\minse\\Desktop\\Programming\\FairThresholdOptimization\\results_updated_0.2_10K.txt", "a") as f:
             f.write(f"\nPerformance for Source: {source}, Detector: {detector}\n")
             f.write(f"Accuracy: {test_accuracy:.4f}\n")
             f.write(f"False Positive Rate (FPR): {test_fpr:.4f}\n")
