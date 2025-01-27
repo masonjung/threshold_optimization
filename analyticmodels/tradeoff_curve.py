1f414a63d679cfcb8745260b17015de5fe449499
masonjung <msjung@mit.edu>
2025-01-27 13:22:29 -0500

    viz_curve

--- analyticmodels/tradeoff_curve.py
+++ analyticmodels/tradeoff_curve.py
@@ -11,7 +11,7 @@ detector_regex = re.compile(r"Performance for Source: .*?, Detector: (.*?)\n")
 accuracy_regex = re.compile(r"Accuracy: (\d+\.\d+)")
 
 # Target detector to focus on
-target_detector = "roberta_base_openai_detector_probability"
+target_detector = "roberta_large_openai_detector_probability"
 
 relaxation_values = []
 accuracies = []
@@ -43,7 +43,7 @@ sorted_data = sorted(zip(relaxation_values, accuracies))
 relaxation_values, accuracies = zip(*sorted_data)
 
 # Define x-ticks WITHOUT 0.2 included
-xtick_values = [1.0, 0.5, 0.3, 0.15, 0.1]  # Notice 0.2 is removed here
+xtick_values = [1.0, 0.5, 0.3, 0.25, 0.2, 0.15, 0.1]  # Notice 0.2 is removed here
 xtick_labels = [f"{x:.2f}" for x in xtick_values]
 
 # Create figure
@@ -69,7 +69,7 @@ plt.axvline(
 plt.yticks(fontsize=16)
 # Use custom x-ticks and rotate
 plt.xticks(xtick_values, xtick_labels, rotation=45, fontsize=16)
-plt.title("Relaxed Fairness on the Accuracy-Relaxation Curve", fontsize=25)
+plt.title("Relaxed Fairness on the Accuracy-Fairness Tradeoff", fontsize=25)
 plt.xlabel("Fairness relaxation", fontsize=20)
 plt.gca().invert_xaxis()  # Invert x-axis to start from 1.0
 plt.ylabel("Accuracy", fontsize=20)
